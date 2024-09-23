0fe45d351d93204f39e4e87cf70d1a2f1c6cf7f9
Inès Boisson <ines.boisson@epfedu.fr>
2024-09-23 16:16:49 +0200

    exercice 3 TP0

---
+++ TP0_Calculaor_exo3/src/tp0_calculaor_exo3/TP0_Calculaor_exo3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_calculaor_exo3;
+
+/**
+ *
+ * @author Inès
+ */
+public class TP0_Calculaor_exo3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
