d4596a4e405ec5a63ab6d3e762e21c0afcedbff6
Inès Boisson <ines.boisson@epfedu.fr>
2024-09-23 17:37:35 +0200

    fin exercice 3 TP0

--- TP0_Calculaor_exo3/src/tp0_calculaor_exo3/TP0_Calculaor_exo3.java
+++ TP0_Calculaor_exo3/src/tp0_calculaor_exo3/TP0_Calculaor_exo3.java
@@ -4,6 +4,8 @@
  */
 package tp0_calculaor_exo3;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Inès
@@ -15,6 +17,51 @@ public class TP0_Calculaor_exo3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    System.out.println("Please enter the operator:");
+    System.out.println("1) add");
+    System.out.println("2)substract");
+    System.out.println("3) multiply");
+    System.out.println("4) divide");
+    System.out.println("5) modulo");
+    int operateur;
+    double result;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Choisissez une de ces operations :");
+    operateur=sc.nextInt();
+    System.out.println("entrez une premiere valeur");
+    double operande1=sc.nextDouble();
+    System.out.println("entres une 2e valeur");
+    double operande2=sc.nextDouble();
+    if (operateur==1){
+        result= operande1+operande2;
+        System.out.println("resultat : "+result);
+    }
+    else if (operateur==2) {
+        result= operande1-operande2;
+        System.out.println("resultat : "+result);
+    }
+    else if(operateur==3) {
+        result= operande1*operande2;
+        System.out.println("resultat : "+result);
+    }
+    else if (operateur==4) {
+        result=operande1/operande2;
+        System.out.println("resultat : "+result);    
+    }
+    else if (operateur==5) {
+        result=operande1%operande2;
+        System.out.println("resultat : "+result);
+    }
+    else {
+        System.out.println("mettez qqc entre 1 et 5");
+    }
+    
+    }
+
+    private static class enter {
+
+        public enter() {
+        }
     }
     
 }
