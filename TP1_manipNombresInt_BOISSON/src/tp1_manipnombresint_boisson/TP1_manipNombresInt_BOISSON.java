ba5769afba02fdb417e40ac3f9640ed80d3a813c
Inès Boisson <ines.boisson@epfedu.fr>
2024-09-23 18:03:24 +0200

    TP1 exo 1

--- TP1_manipNombresInt_BOISSON/src/tp1_manipnombresint_boisson/TP1_manipNombresInt_BOISSON.java
+++ TP1_manipNombresInt_BOISSON/src/tp1_manipnombresint_boisson/TP1_manipNombresInt_BOISSON.java
@@ -4,6 +4,8 @@
  */
 package tp1_manipnombresint_boisson;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Inès
@@ -15,6 +17,29 @@ public class TP1_manipNombresInt_BOISSON {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("entrez une premiere variable");
+    int var1 = sc.nextInt();
+    System.out.println("entrez une 2e variabe");
+    int var2 = sc.nextInt();
+    System.out.println("vos variables sont "+var1+" et "+var2);
+    int somme;
+    int difference;
+    int produit;
+    somme = var1+var2;
+    System.out.println("la somme est "+somme);
+    difference= var1-var2;
+    System.out.println("la difference est "+difference);
+    produit=var1*var2;
+    System.out.println("le produit est "+produit);
+    int quotient;
+    quotient=var1/var2; // diviesion euclidienne
+    int reste;
+    reste=var1%var2;
+    System.out.println("le qutient est "+quotient+" et le rest est "+reste);
+    
+    
+    
     }
     
 }
