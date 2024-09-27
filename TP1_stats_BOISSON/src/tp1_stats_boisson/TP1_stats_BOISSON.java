431c1e5cebac22d953425cfee10552e9b965de99
Inès Boisson <ines.boisson@epfedu.fr>
2024-09-27 17:14:18 +0200

    TP1 exo 4

--- TP1_stats_BOISSON/src/tp1_stats_boisson/TP1_stats_BOISSON.java
+++ TP1_stats_BOISSON/src/tp1_stats_boisson/TP1_stats_BOISSON.java
@@ -4,6 +4,9 @@
  */
 package tp1_stats_boisson;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Inès
@@ -15,6 +18,32 @@ public class TP1_stats_BOISSON {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         // Créer un scanner pour lire l'entrée de l'utilisateur
+        Scanner sc = new Scanner(System.in);
+        
+        // Demander à l'utilisateur de saisir un nombre entier m
+        System.out.println("Saisissez un nombre entier pour le nombre de lancers : ");
+        int m = sc.nextInt();  // Lire le nombre de lancers
+        
+        // Créer un tableau de 6 entiers pour stocker les résultats (de 0 à 5)
+        int[] resultats = new int[6];  // Les cases sont initialisées à 0 par défaut
+        
+        // Créer un générateur de nombres aléatoires
+        Random generateurAleat = new Random();
+        
+        // Boucle de m itérations pour tirer un nombre aléatoire
+        for (int i = 0; i < m; i++) {
+            int lancer = generateurAleat.nextInt(6);  // Génère un nombre entre 0 et 5
+            resultats[lancer]++;  // Incrémenter la case correspondante au résultat
+        }
+        
+        // Afficher les résultats en pourcentages
+        System.out.println("Distribution des lancers apres " + m + " lancers (en pourcentage) :");
+        for (int i = 0; i < resultats.length; i++) {
+            double pourcentage = ((double) resultats[i] / m) * 100;  // Calculer le pourcentage
+            System.out.printf("Face %d : %.2f%%\n", i, pourcentage);  // Afficher avec 2 décimales
+        }
+        
+       
     }
-    
 }
