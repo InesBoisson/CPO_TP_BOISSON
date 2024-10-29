2ac05027fd87be66496ded30550f330eefadbb6b
Inès Boisson <ines.boisson@epfedu.fr>
2024-10-29 22:54:14 +0100

    TP2 exo 4

--- TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Voiture.java
+++ TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Voiture.java
@@ -9,5 +9,20 @@ package tp2_relation_1_boisson;
  * @author Inès
  */
 public class Voiture {
+    public String modele;
+    public String marque;
+    public int puissanceCV;
+    public Personne proprietaire;
+
+    // Constructeur
+    public Voiture(String modele, String marque, int puissanceCV) {
+        this.modele = modele;
+        this.marque = marque;
+        this.puissanceCV = puissanceCV;
+        this.proprietaire = null;
+    }
+    public String toString() {
+        return "Voiture [Modele :" + modele + ", Marque :" + marque + ", PuissanceCV :" + puissanceCV + "]";
+    }
     
 }
