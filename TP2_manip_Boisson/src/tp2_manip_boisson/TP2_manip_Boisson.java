ffeac0d10e817cd958614c43768429b3aff634a9
Inès Boisson <ines.boisson@epfedu.fr>
2024-10-29 22:24:29 +0100

    TP2 exo 3

--- TP2_manip_Boisson/src/tp2_manip_boisson/TP2_manip_Boisson.java
+++ TP2_manip_Boisson/src/tp2_manip_boisson/TP2_manip_Boisson.java
@@ -15,6 +15,24 @@ public class TP2_manip_Boisson {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Tartiflette assiette1 = new Tartiflette(500) ;
+        Tartiflette assiette2 = new Tartiflette(600) ;
+        Tartiflette assiette3 = assiette2 ; 
+        System.out.println("nb de calories de Assiette 2 : " +
+        assiette2.nbCalories) ;
+        System.out.println("nb de calories de Assiette 3 : " +
+        assiette3.nbCalories) ;
+        Tartiflette temp = assiette1;
+        assiette1 = assiette2;
+        assiette2= temp;
+        
+        Moussaka[] tabMoussaka = new Moussaka[10];
+        for (int i = 0; i < tabMoussaka.length; i++) {
+            tabMoussaka[i] = new Moussaka(100 + i * 10);
+        }
+
+
+
     }
     
 }
