fcb48a2050e5565070b236a64527758cb7a7ff89
Inès Boisson <ines.boisson@epfedu.fr>
2024-10-29 22:54:28 +0100

    TP2 exo 4

--- TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Personne.java
+++ TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Personne.java
@@ -9,5 +9,21 @@ package tp2_relation_1_boisson;
  * @author Inès
  */
 public class Personne {
+    public String nom;
+    public String prenom;
+    public int nbVoitures;
+    public Voiture[] liste_voitures;
+    
+    public Personne (String nom, String prenom){
+        this.nom=nom;
+        this.prenom=prenom;
+        this.nbVoitures = 0;
+        liste_voitures = new Voiture [3] ;
+    }
+
+    @Override
+    public String toString() {
+        return "Personne [nom :" + nom + ", prenom :" + prenom +"]";
+    }
     
 }
