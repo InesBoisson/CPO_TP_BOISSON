4a87907bb78b32301538679141cfde52b68a72a9
Inès Boisson <ines.boisson@epfedu.fr>
2024-11-06 14:12:20 +0100

    TP2 exo 4

--- TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Personne.java
+++ TP2_relation_1_Boisson/src/tp2_relation_1_boisson/Personne.java
@@ -25,5 +25,26 @@ public class Personne {
     public String toString() {
         return "Personne [nom :" + nom + ", prenom :" + prenom +"]";
     }
-    
+ public boolean ajouter_voiture(Voiture voiture_a_ajouter) {
+    // Vérifier si la voiture a déjà un propriétaire
+    if (voiture_a_ajouter.proprietaire != null) {
+        System.out.println("Erreur : La voiture appartient deja a " + voiture_a_ajouter.proprietaire.nom + ".");
+        return false;
+    }
+
+    // Vérifier si le tableau est plein
+    if (nbVoitures >= liste_voitures.length) {
+        System.out.println("Erreur : La liste des voitures est pleine pour " + prenom + ".");
+        return false;
+    }
+
+    // Ajout de la voiture
+    liste_voitures[nbVoitures] = voiture_a_ajouter;
+    voiture_a_ajouter.proprietaire = this; // Définir le propriétaire de la voiture
+    nbVoitures++; // Incrémenter le nombre de voitures
+
+    System.out.println("Voiture ajoutee avec succes pour " + prenom + ".");
+    return true;
+ }
+
 }
