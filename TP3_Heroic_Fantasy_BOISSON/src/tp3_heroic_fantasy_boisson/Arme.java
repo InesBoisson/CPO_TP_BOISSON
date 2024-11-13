a57636ea95c36942e490e90768c43307d809d0c3
Inès Boisson <ines.boisson@epfedu.fr>
2024-11-13 18:36:59 +0100

    TP3 exo 1

--- TP3_Heroic_Fantasy_BOISSON/src/tp3_heroic_fantasy_boisson/Arme.java
+++ TP3_Heroic_Fantasy_BOISSON/src/tp3_heroic_fantasy_boisson/Arme.java
@@ -1,4 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package tp3_heroic_fantasy_boisson;
+
+/**
+ *
+ * @author Inès
+ */
 
 // Classe abstraite Arme
 public abstract class Arme {
